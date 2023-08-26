f673e7ae23a4722581a2c0ed72ad841d2806e26f
qwacko <james@jmcdowall.com>
2023-08-26 19:29:49 +0000

    Fixed signup schema.

--- src/lib/schema/signupSchema.ts
+++ src/lib/schema/signupSchema.ts
@@ -1,15 +1,20 @@
 import { z } from 'zod';
 
-export const signupSchema = z.object({
-	username: z.string().min(3),
-	password: z
-		.string()
-		.min(8)
-		.regex(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$/, {
-			message:
-				'Password must contain at least one uppercase letter, one lowercase letter, and one number.'
-		}),
-	confirmPassword: z.string().min(8)
-});
+export const signupSchema = z
+	.object({
+		username: z.string().min(3),
+		password: z
+			.string()
+			.min(8)
+			.regex(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$/, {
+				message:
+					'Password must contain at least one uppercase letter, one lowercase letter, and one number.'
+			}),
+		confirmPassword: z.string().min(8)
+	})
+	.refine((data) => data.password === data.confirmPassword, {
+		message: 'Passwords must match',
+		path: ['confirmPassword']
+	});
 
 export type signupSchemaType = typeof signupSchema;
