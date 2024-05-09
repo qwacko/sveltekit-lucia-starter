83f57d0a18bf0aa5a44d2810f2e003b4e152d4db
james <james@jmcdowall.com>
2024-05-09 21:23:20 +0000

    Package Version Updates

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -67,7 +67,7 @@ if (!self.define) {
     });
   };
 }
-define(['./workbox-39884a30'], (function (workbox) { 'use strict';
+define(['./workbox-dcc48fc1'], (function (workbox) { 'use strict';
 
   self.addEventListener('message', event => {
     if (event.data && event.data.type === 'SKIP_WAITING') {
@@ -82,7 +82,7 @@ define(['./workbox-39884a30'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/",
-    "revision": "0.b9fmp8sd8h8"
+    "revision": "0.g5mvojt9csg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/"), {
