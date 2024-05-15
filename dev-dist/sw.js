6f138547038f0abb5c2a74b389e5d599972f458d
james <james@jmcdowall.com>
2024-05-15 03:40:11 +0000

    Added unstorage and sse

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-dcc48fc1'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/",
-    "revision": "0.1blgjgqd61"
+    "revision": "0.h1tddo2bfc8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/"), {
