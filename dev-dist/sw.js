2c01cb76c8607c2a9f4e80f78dfd1e9def43768e
james <james@jmcdowall.com>
2024-09-04 10:13:24 +0000

    Dependency Update

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-dcc48fc1'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/",
-    "revision": "0.7r8o415hlho"
+    "revision": "0.bpvav1n8s58"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/"), {
