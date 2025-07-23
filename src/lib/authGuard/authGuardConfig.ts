b975e3947fe7cb40baf1b713aa081ceb7e5e7741
james <james@jmcdowall.com>
2025-07-23 19:20:29 +0000

    Removed unnecessary logging.

--- src/lib/authGuard/authGuardConfig.ts
+++ src/lib/authGuard/authGuardConfig.ts
@@ -8,7 +8,6 @@ type UserValidationOutput = {
 
 const adminOnlyConfig: RouteConfig<UserValidationOutput> = {
 	check: (data) => {
-		console.log('Admin Check', data);
 		return data.admin ? null : data.user ? '/' : '/login';
 	}
 };
